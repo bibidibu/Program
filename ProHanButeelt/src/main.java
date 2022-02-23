35ce7aaf7a1dbf7849ce94346a854a8b9e85bdf5
Ben <Ben@192.168.1.9>
2022-02-23 06:35:34 -0800

    gg

--- ProHanButeelt/src/main.java
+++ ProHanButeelt/src/main.java
@@ -6,20 +6,21 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-		System.out.println("Ð”Ð°Ñ€Ð°Ð°Ð»Ð°Ð½ Ð¾Ñ€Ñ�Ð¾Ð½ 2 Ñ�Ð»ÐµÐ¼ÐµÐ½Ñ‚ Ð±Ò¯Ñ€ Ð½ÑŒ Ñ�Ð»Ð³Ð°Ð°Ñ‚Ð°Ð¹ Ð±Ò¯Ñ…Ñ�Ð» Ñ‚Ð¾Ð¾Ð½ Ð´Ð°Ñ€Ð°Ð°Ð»Ð°Ð» Ó©Ð³Ó©Ð³Ð´Ó©Ð½Ó©. "+"\nÐ¢Ð¾Ð¾Ð½ÑƒÑƒÐ´ Ð½ÑŒ Ó©Ð³Ñ�Ó©Ð¶ Ñ�Ð²Ð°Ð°Ð´ ÑƒÑ€ÑƒÑƒÐ´Ð²Ð°Ð» Ñ‚Ñ�Ñ€Ñ…Ò¯Ò¯ Ñ…Ñ�Ñ�Ð³Ð¸Ð¹Ð³ Ð¾Ñ€Ð³Ð¸Ð», ÑƒÑ€ÑƒÑƒÐ´Ð°Ð¶ Ñ�Ð²Ð°Ð°Ð´ Ó©Ð³Ñ�Ð²Ó©Ð» Ñ‘Ñ€Ð¾Ð¾Ð» Ð³Ñ�Ðµ."+"\n Ð¢Ð¾Ð¾Ð½ Ð´Ð°Ñ€Ð°Ð°Ð»Ð°Ð» Ñ…Ñ�Ð´Ñ�Ð½ Ð¾Ñ€Ð³Ð¸Ð», Ñ…Ñ�Ð´Ñ�Ð½ Ñ‘Ñ€Ð¾Ð¾Ð» Ò¯Ò¯Ñ�Ð³Ñ�Ñ�Ð½Ð¸Ð¹Ð³ Ð¾Ð»."+"\n Ð”Ð°Ñ€Ð°Ð°Ð»Ð»Ñ‹Ð½ Ñ…Ð°Ð¼Ð³Ð¸Ð¹Ð½ Ñ�Ñ…Ð½Ð¸Ð¹ Ð±Ð¾Ð»Ð¾Ð½ Ñ�Ò¯Ò¯Ð»Ð¸Ð¹Ð½ Ñ‚Ð¾Ð¾ Ð¾Ñ€Ð³Ð¸Ð» Ð±Ð¾Ð»Ð¾Ð½ Ñ‘Ñ€Ð¾Ð¾Ð»Ð´ Ñ‚Ð¾Ð¾Ð»Ð¾Ð³Ð´Ð¾Ñ…Ð³Ò¯Ð¹. ");
-		System.out.println("\nÐ”Ð°Ñ€Ð°Ð°Ð»Ð°Ð»Ñ‹Ð½ Ñ�Ð»ÐµÐ¼ÐµÐ½Ñ‚Ð¸Ð¹Ð½ Ñ‚Ð¾Ð¾Ð³ Ð¾Ñ€ÑƒÑƒÐ»Ð½Ð° ÑƒÑƒ:");
+		System.out.println(" ");
+		System.out.println(":");
 		Scanner scan=new Scanner(System.in);
 		
+		int n = 0;
 		if(n>0&&n<1000) {
 		int[] mas=new int[n];
-		System.out.println("Ð”Ð°Ñ€Ð°Ð°Ð»Ð°Ð»Ñ‹Ð½ Ñ�Ð»ÐµÐ¼ÐµÐ½Ñ‚Ò¯Ò¯Ð´Ð¸Ð¹Ð³ Ð¾Ñ€ÑƒÑƒÐ»Ð½Ð° ÑƒÑƒ:");
+		System.out.println(":");
 		System.out.print("n["+1+"]=");
 		mas[0]=scan.nextInt();
 		for(int i=1; i<n; i++) {
 			System.out.print("n["+(i+1)+"]=");
 	        mas[i]=scan.nextInt();
 			if(mas[i-1]==mas[i]) {
-				System.out.println("Ó¨Ð¼Ð½Ó©Ñ… Ñ‚Ð¾Ð¾Ð½Ð¾Ð¾Ñ� Ó©Ó©Ñ€ Ñ‚Ð¾Ð¾ Ð¾Ñ€ÑƒÑƒÐ»Ð½Ð° ÑƒÑƒ:");
+				System.out.println(":");
 				System.out.print("n["+(i+1)+"]=");
 				mas[i]=scan.nextInt();
 			}
@@ -37,16 +38,16 @@ public class main {
 				bottom=bottom+1;
 			}
 		}
-		System.out.println("Ð”Ð°Ñ€Ð°Ð°Ð»Ð»Ñ‹Ð½ Ð¾Ñ€Ð³Ð¸Ð»Ñ‹Ð½ Ñ‚Ð¾Ð¾:"+top);
-		System.out.println("Ð”Ð°Ñ€Ð°Ð°Ð»Ð»Ñ‹Ð½ Ñ‘Ñ€Ð¾Ð¾Ð»Ñ‹Ð½ Ñ‚Ð¾Ð¾:"+bottom);
+		System.out.println(""+top);
+		System.out.println(""+bottom);
 	}
 		
 		else {
-			System.out.println("1000 Ñ� Ð±Ð°Ð³Ð° Ð­ÐµÑ€Ñ�Ð³ Ð±Ò¯Ñ…Ñ�Ð» Ñ‚Ð¾Ð¾ Ð¾Ñ€ÑƒÑƒÐ»Ð½Ð° ÑƒÑƒ.");
+			System.out.println("");
 		}
 		}
 	catch(Exception e) {
-		System.out.println("Ð­ÐµÑ€Ñ�Ð³ Ð±Ò¯Ñ…Ñ�Ð» Ñ‚Ð¾Ð¾ Ð¾Ñ€ÑƒÑƒÐ»Ð½Ð° ÑƒÑƒ.");
+		System.out.println(".");
 	}
 	}
 	
